63b8a7c71c8e5b736af6f39b87abf978930d7a6f
raveendraHewage <hewagerv@gmail.com>
2020-12-31 09:00:49 +0530

    nineth commit

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.use(express.static('./public'));
 
 todoController(app);
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => console.log(`Listening on port ${port}`));
 console.log("Port 3000 is listning");
